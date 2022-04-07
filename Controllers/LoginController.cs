f295b4e4be798dea669beafd1c148af5a1fd41ea
ozzi0032 <muzair3284@outlook.com>
2022-04-07 15:41:46 +0500

    hmac authentication added

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,47 +1,64 @@
 ﻿using System;
 using System.Net;
+using System.Linq;
 using System.Web.Http;
 using Newtonsoft.Json;
-using SmartStore.Core;
 using SmartStore.Core.Domain.Customers;
-using SmartStore.Core.Logging;
-using SmartStore.Services.Authentication;
 using SmartStore.Services.Customers;
-using SmartStore.Services.Localization;
-using SmartStore.Services.Orders;
+using SmartStore.Web.Framework.WebApi.Caching;
+using BizSol.Tracker.Api.Providers;
+using System.Collections.Generic;
+using SmartStore;
+using SmartStore.Core.Domain.Common;
+using SmartStore.Core.Data;
+using SmartStore.Web.Framework.WebApi.Security;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SmartStore.Web.Framework.WebApi;
+using System.Web;
 
 namespace BizSol.Tracker.Api.Controllers
 {
     public class LoginController : ApiController
     {
-        private readonly IAuthenticationService _authenticationService;
-        private readonly ILocalizationService _localizationService;
-        private readonly IWorkContext _workContext;
+        protected HmacAuthentication _hmac = new HmacAuthentication();
+
         private readonly ICustomerService _customerService;
         private readonly ICustomerRegistrationService _customerRegistrationService;
         private readonly CustomerSettings _customerSettings;
-        private readonly IShoppingCartService _shoppingCartService;
-        private readonly ICustomerActivityService _customerActivityService;
+        private readonly IRepository<GenericAttribute> _genericAttributes;
 
         public LoginController(
-            IAuthenticationService authenticationService,
-            ILocalizationService localizationService,
-            IWorkContext workContext,
             ICustomerService customerService,
             ICustomerRegistrationService customerRegistrationService,
             CustomerSettings customerSettings,
-            IShoppingCartService shoppingCartService,
-            ICustomerActivityService customerActivityService
+            IRepository<GenericAttribute> genericAttributes
             )
         {
-            _authenticationService = authenticationService;
-            _localizationService = localizationService;
-            _workContext = workContext;
             _customerService = customerService;
             _customerRegistrationService = customerRegistrationService;
             _customerSettings = customerSettings;
-            _shoppingCartService = shoppingCartService;
-            _customerActivityService = customerActivityService;
+            _genericAttributes = genericAttributes;
+            
+        }
+
+        public string[] GetCustomerAttributes(int custId)
+        {
+            var attribute = (
+                    from a in _genericAttributes.Table
+                    where a.EntityId == custId && a.KeyGroup == "Customer" && a.Key == WebApiCachingUserData.Key
+                    select a).FirstOrDefault();
+
+            //to get the public and secret key from WebApiCachingUserData aka ---> Keys exposed by Web Api Plugin
+            if (!string.IsNullOrEmpty(attribute.Value))
+            {
+                string[] arr = attribute.Value.SplitSafe("¶");
+                if (arr.Length > 2)
+                {
+                    return arr;
+                }
+            }
+            return null;
         }
 
         [HttpGet]
@@ -94,20 +111,66 @@ namespace BizSol.Tracker.Api.Controllers
                         customer = _customerService.GetCustomerByUsername(userNameOrEmail);
                 }
 
-                _shoppingCartService.MigrateShoppingCart(_workContext.CurrentCustomer, customer);
+                //Get Keys
+                var arr = GetCustomerAttributes(customer.Id);
+                var publicKey = arr[1];
+                var secretKey = arr[2];
+                var url = $"http://localhost:58201/api/Plugin/Login?order=1";
 
-                _authenticationService.SignIn(customer, false);
+                //Generate Token
+                //var signatures = AppAuthProvider.GetAuthorizationToken(publicKey,secretKey,"","get",url);
 
-                _customerActivityService.InsertActivity("PublicStore.Login", _localizationService.GetResource("ActivityLog.PublicStore.Login"), customer);
-                //This is returning JSON String -> later have to work on this
+                //Below is how server side is generating the tokens
+                var timeStamp = DateTime.UtcNow.ToString("o");
+                var context = new WebApiRequestContext
+                {
+                    HttpMethod = "get",
+                    HttpAcceptType = "application/json, text/javascript, */*",
+                    PublicKey = publicKey,
+                    SecretKey = secretKey,
+                    Url = HttpUtility.UrlDecode(new Uri(url).Query).ToLower(),
+                };
+                var messageRepresentation = _hmac.CreateMessageRepresentation(context, null, timeStamp);
+                var signatures = _hmac.CreateSignature(secretKey, messageRepresentation);
+
+                //This is returning JSON String
                 var JsonCustomerModel = JsonConvert.SerializeObject(customer, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
-                return Json(JsonCustomerModel);
+                Dictionary<string, dynamic> response = new Dictionary<string, dynamic>();
+                response.Add("token", signatures);
+                response.Add("timeStamp", timeStamp);
+                response.Add("publicKey", publicKey);
+                response.Add("customer", JsonCustomerModel);
+                return Json(response);
             }
             else
             {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
         }
+
+        [HttpGet]
+        [WebApiAuthenticate]
+        public IHttpActionResult GetOrders(int order)
+        {
+            return Json(new { order });
+        }
+
+        //public async Task MakeHttpReqAsync(string url,string publicKey,string signatures)
+        //{
+        //    var httpClient = new HttpClient();
+        //    var msg = new HttpRequestMessage(HttpMethod.Get, url);
+
+        //    string _time = DateTime.UtcNow.ToString("o"); // 2022-01-16T19:44:04.9378268Z
+        //    msg.Headers.Add("SmartStore-Net-Api-PublicKey", publicKey);
+        //    msg.Headers.Add("SmartStore-Net-Api-Date", _time);
+        //    msg.Headers.Add("Authorization", "SmNetHmac1 " + signatures);
+        //    msg.Headers.Add("Accept-Charset", "UTF-8");
+        //    msg.Headers.Add("Accept", "application/json, text/javascript, */*");
+        //    msg.Headers.Add("Content-Type", "application/json");
+        //    var res = await httpClient.SendAsync(msg);
+
+        //    Console.WriteLine(res);
+        //}
 
     }
 }
---
+++ Providers/AppAuthProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BizSol.Tracker.Api.Providers
+{
+    public class AppAuthProvider
+    {
+        public static string GetAuthorizationToken(string publicKey, string secretKey, string contentMd5Hash, string method,
+            string url)
+        {
+            string accept = "application/json, text/javascript, */*";
+            var timestamp = DateTime.UtcNow.ToString("o");
+            var uri = new Uri(url);     // decode url
+
+            if (uri.Query != null && uri.Query.Length > 0)
+            {
+                url = string.Concat(uri.GetLeftPart(UriPartial.Path), HttpUtility.UrlDecode(uri.Query));
+            }
+
+
+            var messageRepresentation = string.Join("\n",
+                method.ToLower(),
+                contentMd5Hash ?? "",
+                accept.ToLower(),
+                url.ToLower(),
+                timestamp,
+                publicKey.ToLower()
+            );
+            var signature = CreateSignature(secretKey, messageRepresentation);
+            return signature;
+        }
+
+        public static string CreateSignature(string secretKey, string messageRepresentation)
+        {
+            if (string.IsNullOrWhiteSpace(secretKey) || string.IsNullOrWhiteSpace(messageRepresentation))
+                return "";
+
+            string signature;
+            var secretBytes = Encoding.UTF8.GetBytes(secretKey);
+            var valueBytes = Encoding.UTF8.GetBytes(messageRepresentation);
+
+            using (var hmac = new HMACSHA256(secretBytes))
+            {
+                var hash = hmac.ComputeHash(valueBytes);
+                signature = Convert.ToBase64String(hash);
+            }
+            return signature;
+        }
+
+        public static string CreateContentMd5Hash(byte[] content)
+        {
+            string result = "";
+            if (content != null && content.Length > 0)
+            {
+                using (var md5 = MD5.Create())
+                {
+                    byte[] hash = md5.ComputeHash(content);
+                    result = Convert.ToBase64String(hash);
+                }
+            }
+            return result;
+        }
+    }
+}
